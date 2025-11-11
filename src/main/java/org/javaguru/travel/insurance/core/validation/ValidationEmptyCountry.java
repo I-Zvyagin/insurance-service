a1976659ae8dbeec24866b86e2d4ca105d814222
i_zvyagin <vanyazvyaginfr@gmail.com>
2025-11-11 14:17:46 +0300

    Added validation for country and medical risk limit level.

--- src/main/java/org/javaguru/travel/insurance/core/validation/ValidationEmptyCountry.java
+++ src/main/java/org/javaguru/travel/insurance/core/validation/ValidationEmptyCountry.java
@@ -2,7 +2,6 @@ package org.javaguru.travel.insurance.core.validation;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import org.javaguru.travel.insurance.core.repositories.ClassifierValueRepository;
 import org.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.dto.ValidationError;
 import org.springframework.stereotype.Component;
@@ -11,30 +10,24 @@ import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
-class ValidationCountry extends ValidationServiceImpl{
+class ValidationEmptyCountry extends ValidationServiceImpl{
 
-    private final ClassifierValueRepository classifierValueRepository;
     private final ValidationErrorFactory validationErrorFactory;
 
     @Override
     public Optional<ValidationError> validate(TravelCalculatePremiumRequest request) {
-        return (checkCountryIsFilled(request)
+        return (checkCountryNotFilled(request)
                 && checkRiskListContainTravelMedical(request))
-                && !checkCountryIsSupported(request)
-                ? Optional.of(validationErrorFactory.buildError("ERROR_CODE_15"))
+                ? Optional.of(validationErrorFactory.buildError("ERROR_CODE_10"))
                 : Optional.empty();
     }
 
-    private boolean checkCountryIsFilled(TravelCalculatePremiumRequest request) {
-        return request.getCountry() != null && !request.getCountry().isBlank();
+    private boolean checkCountryNotFilled(TravelCalculatePremiumRequest request) {
+        return request.getCountry() == null || request.getCountry().isBlank();
     }
 
     private boolean checkRiskListContainTravelMedical(TravelCalculatePremiumRequest request) {
         return request.getSelectedRisks() != null &&
                 request.getSelectedRisks().contains("TRAVEL_MEDICAL");
-    }
-
-    private boolean checkCountryIsSupported(TravelCalculatePremiumRequest request) {
-        return classifierValueRepository.findByClassifierTitleAndIc("COUNTRY", request.getCountry()).isPresent();
     }
 }
